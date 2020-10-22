8632d2a41b34a6cabcdec2ceba00d4368650dd79
Rodrigo Becker Probst <becker.rodrigo@outlook.com>
2020-10-21 21:56:40 -0300

    configurando o webpack

--- modulo01/frontend/public/script.js
+++
@@ -1,6 +0,0 @@
-/**
- * Babel: Converter (transpilar) código do react para um código que o browsewr entenda
- * Webpack: Pra cada tipo de arquivo (.js, .css, .png) eu vou converter o código de uma maneira diferente
- * 
- * Loaders: babel-loader (.js), css-loader, image-loader, file-loader
- */
